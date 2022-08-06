94ea70b03f15e6938a01f087a537aeacc4052b33
Joshua Bray <josh.c.bray@gmail.com>
2022-08-06 14:30:05 +0100

    Add styling to home Card 1 Desktop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,9 @@ export default function Home(props) {
         <div className="home--overlay">
           <h1>Lorem ipsum dolor</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <LinkButton url="/contact" text="Contact us" />
+          <div className="home--contact">
+            <LinkButton url="/contact" text="Contact us" />
+          </div>
         </div>
       </div>
 
@@ -29,6 +31,31 @@ export default function Home(props) {
         ) : (
           <p>Loading...</p>
         )}
+        <div className="home--c2-card">
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod
+            nisi porta lorem mollis aliquam. Mattis rhoncus urna neque viverra
+            justo nec. Dignissim sodales ut eu sem integer vitae. Varius vel
+            pharetra vel turpis nunc eget lorem dolor. Mauris pharetra et
+            ultrices neque. Urna molestie at elementum eu facilisis sed odio.
+            Enim lobortis scelerisque fermentum dui faucibus. Ullamcorper eget
+            nulla facilisi etiam dignissim diam quis enim lobortis. Pellentesque
+            eu tincidunt tortor aliquam nulla. Tellus cras adipiscing enim eu
+            turpis egestas pretium aenean pharetra. Arcu vitae elementum
+            curabitur vitae nunc sed. Gravida rutrum quisque non tellus orci ac.
+            Quam viverra orci sagittis eu volutpat odio facilisis mauris sit.
+          </p>
+          <ul>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing</li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipiscing</li>
+          </ul>
+          <button>Learn more</button>
+        </div>
       </div>
       <div className="home--c3">
         {props.images ? (
