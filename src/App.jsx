9aab01268796837afa57c06efb0665d3dfc072e0
Joshua Bray <josh.c.bray@gmail.com>
2022-08-06 09:21:28 +0100

    Add fetch data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 
@@ -8,9 +9,20 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [bannerData, setBannerData] = useState([]);
+
   const isDesktop = useMediaQuery({
     query: "(min-device-width: 1200px)",
   });
+
+  const bannerUrl =
+    "https://interview-assessment.api.avamae.co.uk/api/v1/home/banner-details";
+
+  useEffect(() => {
+    fetch(bannerUrl)
+      .then((result) => result.json())
+      .then((data) => setBannerData(data));
+  }, []);
 
   return (
     <div className="App">
