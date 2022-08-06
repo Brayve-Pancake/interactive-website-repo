1b4fea0d095c2fbf64b59bf7e37643b5a26606f2
Joshua Bray <josh.c.bray@gmail.com>
2022-08-06 17:56:25 +0100

    Add home card #4 for mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ export default function Home(props) {
 
   return (
     <div className="home">
-      <div className="home--c1 home--banner">
+      <div className="home--c1">
         {props.images ? (
           <img className="home--banner-one" src={props.images[0].ImageUrl} />
         ) : (
@@ -25,54 +25,112 @@ export default function Home(props) {
         </div>
       </div>
 
-      <div className="home--c2 home--banner">
+      <div className="home--c2">
+        <div className="container-re-order">
+          <div className="col1">
+            {props.images ? (
+              <img
+                className="home--banner-two"
+                src={props.images[1].ImageUrl}
+              />
+            ) : (
+              <p>Loading...</p>
+            )}
+          </div>
+          <div className="home--c2-text col2">
+            <h2 className="home--c2-title">
+              Justo ipsum dolor sit amet, conture et
+            </h2>
+            <p className="home--c2-para">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Euismod nisi porta lorem mollis aliquam. Mattis rhoncus urna neque
+              viverra justo nec. Dignissim sodales ut eu sem integer vitae.
+              Varius vel pharetra vel turpis nunc.
+            </p>
+            <ul className="home--c2-ulist">
+              <li className="home--c2-list">
+                <span>Dolor sit amet consectetur adipisicing elit.</span>
+              </li>
+              <li className="home--c2-list">
+                <span>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing neque.
+                </span>
+              </li>
+              <li className="home--c2-list">
+                <span>Mattis rhoncus urna neque viverra justo.</span>
+              </li>
+              <li className="home--c2-list">
+                <span>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elitusem.
+                </span>
+              </li>
+            </ul>
+            <div className="home--contact home--learn-more">
+              <LinkButton url="/about" text="Learn more" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="home--c3">
         {props.images ? (
-          <img className="home--banner-two" src={props.images[1].ImageUrl} />
+          <img className="home--banner-one c3" src={props.images[2].ImageUrl} />
         ) : (
           <p>Loading...</p>
         )}
-        <div className="home--c2-text">
-          <h2 className="home--c2-title">
-            Justo ipsum dolor sit amet, consectetur amet
-          </h2>
-          <p className="home--c2-para">
+        <div className="home--c3-overlay">
+          <h1>Lorem ipsum dolor sit amet, consectetur adipiscing</h1>
+          <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod
             nisi porta lorem mollis aliquam. Mattis rhoncus urna neque viverra
             justo nec. Dignissim sodales ut eu sem integer vitae. Varius vel
-            pharetra vel turpis nunc.
+            pharetra vel turpis nunc eget lorem dolor. Nisi illuminati porta
+            lorem mollis aliquam. Dignissim sodales ut eu sem integer vitae.
           </p>
-          <ul className="home--c2-ulist">
-            <li className="home--c2-list">
-              <span>Dolor sit amet consectetur adipisicing elit.</span>
-            </li>
-            <li className="home--c2-list">
-              <span>
-                Lorem ipsum dolor sit, amet consectetur adipisicing neque.
-              </span>
-            </li>
-            <li className="home--c2-list">
-              <span>Mattis rhoncus urna neque viverra justo.</span>
-            </li>
-            <li className="home--c2-list">
-              <span>
-                Lorem ipsum dolor sit amet consectetur adipisicing elitusem.
-              </span>
-            </li>
-          </ul>
-          <div className="home--learn-more">
-            <LinkButton url="/about" text="Learn more" />
+          <div className="home--contact c3">
+            <LinkButton url="/about" text="Log in" />
           </div>
         </div>
       </div>
-      <div className="home--c3">
-        {props.images ? (
-          <img className="home--banner-one" src={props.images[2].ImageUrl} />
-        ) : (
-          <p>Loading...</p>
-        )}
+      <div className="home--c4">
+        <div className="home--c4-head">
+          <h2>Dolor ipsum amet, lorem loreum non</h2>
+          <h4>Nisi illuminati porta lorem mollis</h4>
+        </div>
+
+        <p>
+          <span className="first-para">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod
+            nisi porta lorem mollis aliquam. Mattis rhoncus urna neque viverra
+            justo nec. Dignissim sodales ut eu sem integer vitae. Varius vel
+            pharetra vel turpis nunc eget lorem dolor. Nisi illuminati porta
+            lorem mollis aliquam. Dignissim sodales ut eu sem integer vitae.
+          </span>
+          <br></br>
+          <br></br>
+          Eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod
+          nisi porta lorem mollis aliquam. Mattis rhoncus urna neque viverra
+          justo nec. Dignissim sodales ut eu sem integer vitae. Varius vel
+          pharetra vel turpis nunc eget lorem dolor. Nisi illuminati porta lorem
+          mollis aliquam. Dignissim sodales ut eu sem integer vitae.
+          <br></br>
+          <br></br>
+          Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Euismod nisi porta lorem mollis
+          aliquam. Mattis rhoncus urna neque viverra justo nec. Dignissim
+          sodales ut eu sem integer vitae. Dignissim sodales ut eu sem integer
+          vitae. Varius vel pharetra vel turpis nunc eget lorem dolor. Nisi
+          illuminati porta lorem mollis aliquam. Dignissim sodales ut eu sem
+          integer vitae.
+        </p>
+        <br></br>
+        <div className="home--contact c4">
+          <LinkButton url="/contact" text="Contact us" />
+        </div>
       </div>
-      <div className="home--c4"></div>
     </div>
   );
 }
