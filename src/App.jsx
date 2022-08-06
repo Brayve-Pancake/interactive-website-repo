8ff2afcbc7fe79b99c13bddb6d0698a834fe51db
Joshua Bray <josh.c.bray@gmail.com>
2022-08-06 08:47:42 +0100

    Add Router and connect routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Routes, Route } from "react-router-dom";
+
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -8,6 +10,13 @@ function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
