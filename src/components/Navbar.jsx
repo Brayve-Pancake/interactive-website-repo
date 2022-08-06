28f0e789ad238617791fe81064e7fa539f78eb83
Joshua Bray <josh.c.bray@gmail.com>
2022-08-06 08:49:14 +0100

    Add links to navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <div>
       <h1>Navbar</h1>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
     </div>
   );
 }
