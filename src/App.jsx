e2be85e8b6fa6de7524cc6aadf35f9489c9261e9
Joshua Bray <josh.c.bray@gmail.com>
2022-08-06 08:39:13 +0100

    Create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
