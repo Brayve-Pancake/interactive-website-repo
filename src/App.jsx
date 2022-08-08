f0c88dd5d302c842f214590f8a4127711dc3add0
Joshua Bray <josh.c.bray@gmail.com>
2022-08-08 15:39:39 +0100

    Render Home

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Home />
       <Footer />
     </div>
   );
